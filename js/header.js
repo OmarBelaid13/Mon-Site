4165adc34d56f14433d211c353fded34fc7dfff6
OmarBelaid13 <o.belaid@gmail.com>
2022-06-09 15:55:09 +0200

    Ajout du lien .sec01 sur la flèche dans le header

--- js/header.js
+++ js/header.js
@@ -37,3 +37,16 @@ $("nav a").click(function (event) {
   );
   event.preventDefault();
 });
+
+$("a").click(function (event) {
+  let id = $(this).attr("href");
+  let offset = 70;
+  let target = $(id).offset().top - offset;
+  $("html, body").animate(
+    {
+      scrollTop: target,
+    },
+    500
+  );
+  event.preventDefault();
+});
